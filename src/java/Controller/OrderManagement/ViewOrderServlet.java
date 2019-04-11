648b521f3675cb618c4ced6c45374d3b192fd3f4
mast3 <mast3@LAPTOP-UI5APF8K>
2019-04-11 22:34:56 +0800

    Updated

---
+++ src/java/Controller/OrderManagement/ViewOrderServlet.java
@@ -0,0 +1,145 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller.OrderManagement;
+
+import Controller.MealManagement.*;
+import Model.Meal;
+import Model.Mealfood;
+import Model.Studentorder;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+
+/**
+ *
+ * @author mast3
+ */
+
+@WebServlet(name = "ViewOrderServlet", urlPatterns = {"/ViewOrderServlet"})
+public class ViewOrderServlet extends HttpServlet {
+    @PersistenceContext
+    EntityManager em;
+    @Resource
+    UserTransaction utx;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+         HttpSession session = request.getSession(false);
+        
+        String permission = "";
+        String previousUrl = "";
+        
+        try {
+            permission = (String) session.getAttribute("permission");
+            
+            if (permission == null) {
+                request.setAttribute("errorMsg", "Please login.");
+                request.getRequestDispatcher("login.jsp").forward(request, response);
+                return;
+            }
+            
+        } catch (NullPointerException ex) {
+            request.setAttribute("errorMsg", "Please login.");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            return;
+        }
+
+        // If user is not logged in, redirect to login page
+        // Allow student only
+        if (!permission.equalsIgnoreCase("student")) {
+            request.setAttribute("errorMsg", "You are not allowed to visit that page.");
+            request.getRequestDispatcher("login.jsp").forward(request, response);
+            return;
+        } else {
+            
+            Studentorder so = new Studentorder();
+            String orderId = "";
+            
+            try {
+                orderId = request.getParameter("");
+                so = em.find(Studentorder.class, orderId);
+            } catch (Exception e) {
+                // If exception is thrown, just redirect student to dashboard
+                response.sendRedirect("dashboardStudent.jsp");
+            }
+
+            try {
+
+                // Set studentOrder to session so it can be displayed
+               session.setAttribute("studentOrder", so);
+                
+                request.getRequestDispatcher("viewOrder.jsp").forward(request, response);
+                return;
+
+            } catch (Exception e) {
+                System.out.println("Could not obtain meal list: " + e.getMessage());
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
